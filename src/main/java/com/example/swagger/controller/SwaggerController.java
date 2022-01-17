08937623bb8dd30bca9c34facf47149d258b3b02
koboseok <goboseok7@gmail.com>
2022-01-17 17:17:57 +0900

    update

--- src/main/java/com/example/swagger/controller/SwaggerController.java
+++ src/main/java/com/example/swagger/controller/SwaggerController.java
@@ -3,6 +3,7 @@ package com.example.swagger.controller;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.HashMap;
 import java.util.Map;
 
-@Api(tags = {"회원가입 API"})
+@Api(tags = {"API 문서 가이드"})
 @RequestMapping("/member/api")
 @RestController
 public class SwaggerController {
@@ -20,7 +21,16 @@ public class SwaggerController {
     public Map<String,String> signUp(@ApiParam(name = "first param", value = "first value", readOnly = true) String input){
         Map<String, String> result = new HashMap<>();
         result.put("author", "koboseok");
-        result.put("content", "V1 API 내용");
+        result.put("content", "V1 API 회원가입");
+        return result;
+    }
+
+    @ApiOperation(value = "게시판", notes = "게시판!!")
+    @GetMapping(value = "/board")
+    public Map<String,String> board(@ApiParam(name = "first param", value = "first value", readOnly = true) String input){
+        Map<String, String> result = new HashMap<>();
+        result.put("author", "koboseok");
+        result.put("content", "V1 API 게시판 내용");
         return result;
     }
 }
